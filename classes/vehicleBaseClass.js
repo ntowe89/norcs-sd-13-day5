325a85ff92e30ea0c94d0ce1dbdd2e56978f64c7
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2023-09-09 10:27:45 -0500

    add activity

---
+++ classes/vehicleBaseClass.js
@@ -0,0 +1,9 @@
+// Add a new class named Vehicle to the file.
+// Add a constructor.
+// Add five properties that show the different aspects of the vehicles.
+// make, model, color, price, etc
+// Add methods to three properties that extend the functionality of those vehicles.
+//What can a car do?
+//drive
+//stop
+//start / turn on / turn off
